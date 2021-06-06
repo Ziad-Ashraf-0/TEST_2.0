514634ba3bc78b1e1c91329e1e20cff797138f94
Hagar123-cell <85418088+Hagar123-cell@users.noreply.github.com>
2021-06-06 03:00:59 +0200

    adding delay code
    delay code

--- GPS Project.c
+++ GPS Project.c
@@ -101,3 +101,29 @@ delayUs(0);
 GPIO_PORTA_DATA_R = 0;
 delayUs(40);
 }
+/* delay n milliseconds (16 MHz CPU clock) */
+void delayMs(int n)
+{
+int i, j;
+for(i = 0 ; i < n; i++)
+for(j = 0; j < 3180; j++)
+{} /* do nothing for 1 ms */
+}
+/* delay n microseconds (16 MHz CPU clock) */
+void delayUs(int n)
+{
+int i, j;
+for(i = 0 ; i < n; i++)
+for(j = 0; j < 3; j++)
+{} /* do nothing for 1 us */
+}
+
+
+/* This function is called by the startup assembly code to perform system specific
+initialization tasks. */
+void SystemInit(void)
+{
+/* Grant coprocessor access */
+/* This is required since TM4C123G has a floating point coprocessor */
+SCB->CPACR |= 0x00f00000;
+}
