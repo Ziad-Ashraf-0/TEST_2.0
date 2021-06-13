3c3dade9ee9e63402f7b7cfbf2e20324951b44a5
Zidan0 <42922883+Zidan0@users.noreply.github.com>
2021-06-13 23:41:45 +0200

    Update GPS Project.c

--- GPS Project.c
+++ GPS Project.c
@@ -1,9 +1,12 @@
+//liberaries
 #include "stdint.h"
 #include "string.h"
 #include "math.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include "C:/Keil/EE319Kware/inc/tm4c123gh6pm.h"
+
+//defines
 #define RED 0x02
 #define BLUE 0x04
 #define GREEN 0x08
@@ -11,6 +14,7 @@
 #define BUFFER_LENGTH 127
 #define pi 3.14159265358979323846
 
+//prototypes
 void delayMs(int n);
 void delayUs(int n);
 void LCD_command(unsigned char command);
@@ -27,28 +31,24 @@ int main(){
 	initPORTF();
 	LCD_init();
 	UART5Init();
-	
-	
-	while(1){
-		       
-		    GPIO_PORTF_DATA_R = GREEN;			
-		    LCD_command(1); /* clear display */
-		    LCD_command(0x80); /* lcd cursor location */
-		    delayMs(1000);
-			  getData(&lantiude1,&longitude1); 
-		    delayMs(3000);
-	     	getData(&lantiude2,&longitude2);
-	      delayMs(3000);
-	      calcDistance = distance( lantiude1, longitude1, lantiude2,  longitude2);
-		    delayMs(3000);
-		    
-		    sprintf(distance1 , "%lf" , calcDistance);
-		    delayMs(1000);
-		    LCD_data(distance1[0]);
-		    LCD_data(distance1[1]);
-		    LCD_data(distance1[2]);
-		    LCD_data(distance1[3]);
-				LCD_data(distance1[4]);
-	
+	while(1)
+	{	       
+		GPIO_PORTF_DATA_R = GREEN;			
+		LCD_command(1); /* clear display */
+		LCD_command(0x80); /* lcd cursor location */
+		delayMs(1000);
+		getData(&lantiude1,&longitude1); 
+		delayMs(3000);
+		getData(&lantiude2,&longitude2);
+		delayMs(3000);
+		calcDistance = distance( lantiude1, longitude1, lantiude2,  longitude2);
+		delayMs(3000);
+		sprintf(distance1 , "%lf" , calcDistance);
+		delayMs(1000);
+		LCD_data(distance1[0]);
+		LCD_data(distance1[1]);
+		LCD_data(distance1[2]);
+		LCD_data(distance1[3]);
+		LCD_data(distance1[4]);
 	}		
 }
