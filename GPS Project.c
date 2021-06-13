d93c8829070bdfd9cb653c60e62bef15ca5ec200
omarwaels <85417842+omarwaels@users.noreply.github.com>
2021-06-13 14:44:35 -0700

    Adding distance function
    Adding (distance  ,deg2rad  ,rad2deg)

--- GPS Project.c
+++ GPS Project.c
@@ -21,6 +21,28 @@ double longitude1;
 double lantiude2;
 double longitude2;
 
+double deg2rad(double deg) {
+  return (deg * pi / 180);
+}
+double rad2deg(double rad) {
+  return (rad * 180 / pi);
+} 
+	
+double distance(double lat1, double lon1, double lat2, double lon2) {
+  double theta, dist;
+  if ((lat1 == lat2) && (lon1 == lon2)) {
+    return 0;
+  }
+  else {
+    theta = lon1 - lon2;
+    dist = sin(deg2rad(lat1)) * sin(deg2rad(lat2)) + cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * cos(deg2rad(theta));
+    dist = acos(dist);
+    dist = rad2deg(dist);
+    dist = dist * 60 * 1.1515;
+    dist = dist * 1609.344;
+    return (dist);
+  }
+}
 int main(){
 	double calcDistance;
 	char distance1[10];
