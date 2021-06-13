dd4d591c56cd0fe222d2b2d29844297647fa400a
Hagar123-cell <85418088+Hagar123-cell@users.noreply.github.com>
2021-06-13 23:55:59 +0200

    Update GPS Project.c
    adding lcd part

--- GPS Project.c
+++ GPS Project.c
@@ -250,6 +250,45 @@ int main(){
 		LCD_data(distance1[4]);
 	}		
 }
+void LCD_init(void){
+  SYSCTL_RCGCGPIO_R |= 0x00000003;
+	GPIO_PORTA_DIR_R = 0xE0;     //set PORTA pin 7-5 as output
+	GPIO_PORTA_DEN_R = 0xE0;     //set PORTA pin 7-5 as digital pins
+	GPIO_PORTB_DIR_R = 0xFF;     //set all PORTB pins as output for data
+	GPIO_PORTB_DEN_R = 0xFF;     //set all PORTB pins as digital pins
+	delayMs(20); /* initialization sequence */
+	LCD_command(0x30);
+  delayMs(5);
+  LCD_command(0x30);
+  delayUs(100);
+  LCD_command(0x30);
+  LCD_command(0x38); // set 8-bit data, 2-line, 5x7 font 
+  LCD_command(0x06); // move cursor right */
+  LCD_command(0x01); //clear screen, move cursor to home 
+  LCD_command(0x0F); // turn on display, cursor blinking *
+}
+
+void LCD_command(unsigned char command)
+{
+GPIO_PORTA_DATA_R = 0; /* RS = 0, R/W = 0 */
+GPIO_PORTB_DATA_R = command;
+GPIO_PORTA_DATA_R = 0x80; /* pulse E */
+delayUs(0);
+GPIO_PORTA_DATA_R = 0;
+if (command < 4)
+delayMs(2); /* command 1 and 2 needs up to 1.64ms */
+else
+delayUs(40); /* all others 40 us */
+}
+void LCD_data(unsigned char data)
+{
+GPIO_PORTA_DATA_R = 0x20; /* RS = 1, R/W = 0 */
+GPIO_PORTB_DATA_R = data;
+GPIO_PORTA_DATA_R = 0x80 | 0x20; /* pulse E */
+delayUs(0);
+GPIO_PORTA_DATA_R = 0;
+delayUs(40);
+}
 
 uint8_t UART5_ReadAvailable(void){
 		return ((UART5_FR_R&UART_FR_RXFE) == UART_FR_RXFE) ? 0 : 1;
