72a0f3ac75df05c9bdb8527ef714f3e61dcc4604
Hebaadelenan <85418852+Hebaadelenan@users.noreply.github.com>
2021-06-13 23:44:49 +0200

    Update GPS Project.c

--- GPS Project.c
+++ GPS Project.c
@@ -25,6 +25,38 @@ double longitude1;
 double lantiude2;
 double longitude2;
 
+void initPORTF(void){ 
+	
+		SYSCTL_RCGCGPIO_R |= 0x00000020;      // activate clock for port F        
+		while ((SYSCTL_PRGPIO_R&0x20) == 0){};// allow time for clock to start
+		GPIO_PORTF_LOCK_R = 0x4C4F434B;       // unlock GPIO Port F
+    GPIO_PORTF_CR_R = 0x1F;               // allow changes to PF4-0
+		GPIO_PORTF_AMSEL_R = 0x00;            // disable analog on PF
+		GPIO_PORTF_PCTL_R = 0x00000000;       // 
+		GPIO_PORTF_DIR_R = 0x0E;   	         	// PF3-1 out
+		GPIO_PORTF_AFSEL_R = 0x00;            // disable alt funct on PF7-0
+    GPIO_PORTF_PUR_R = 0x11;              // enable pull-up on PF0, PF4
+		GPIO_PORTF_DEN_R = 0x1F;              // enable digital I/O on PF4-0		
+	}
+	
+	void UART5Init(){
+	  SYSCTL_RCGCUART_R |= 0x20;
+		SYSCTL_RCGCGPIO_R |= 0x10;
+		UART5_CTL_R &= ~UART_CTL_UARTEN;
+		//Set BRD
+		//BR = 9600 bits/sec
+		//16*10*6 / (16*9600)= 104.16667
+		UART5_IBRD_R = 104;
+		UART5_FBRD_R = 11;
+		
+		UART5_LCRH_R = (UART_LCRH_WLEN_8 | UART_LCRH_FEN);
+		UART5_CTL_R |= (UART_CTL_UARTEN | UART_CTL_RXE | UART_CTL_TXE);
+		
+		GPIO_PORTE_AFSEL_R |= 0x30;
+		GPIO_PORTE_PCTL_R = 0x00110000;
+	  GPIO_PORTE_DEN_R |= 0x30;
+	}
+
 int main(){
 	double calcDistance;
 	char distance1[10];
